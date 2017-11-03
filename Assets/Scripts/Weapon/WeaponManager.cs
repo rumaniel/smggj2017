e3f64bec395afd88d1ad6715200350e02d9c76f8
Ruman <ruman@bagelcode.com>
2017-11-04 03:23:08 +0900

    do weapon manager

--- Assets/Scripts/Weapon/WeaponManager.cs
+++ Assets/Scripts/Weapon/WeaponManager.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class WeaponManager : MonoSingleton<WeaponManager> 
 {
-	public MonoObjectPool bulletList;
+	public List<MonoObjectPool> bulletList; 
 
 	public Weapon GetWeapon(Defines.WeaponType weaponType, bool isPlayer = false)
 	{
+		Weapon weapon = new Weapon(bulletList[(int)weaponType]);
+
 		switch (weaponType)
 		{
-			// case Defines.WeaponType.Normal:
-			// 	bulletList[0]
+			case Defines.WeaponType.Normal:
+				// set up things
+				break;
+			default:
+				break;
+				
 		}
 		// todo
-		return null;
+		return weapon;
 	}
 
 

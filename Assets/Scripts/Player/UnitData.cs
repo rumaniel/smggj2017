1d0004ea3762fa6f6e3a768bd44943c9c164a804
MW Lee <ionizn@gmail.com>
2017-11-05 04:11:02 +0900

    Add Healt guage script

--- Assets/Scripts/Player/UnitData.cs
+++ Assets/Scripts/Player/UnitData.cs
@@ -4,18 +4,33 @@ using System.Collections.Generic;
 
 public class UnitData : MonoBehaviour 
 {
-	public float health;
+    public float health;
+    public float maxHealth;
+    public SpriteRenderer healthGauge;
 
     public void SetHealth(float health)
     {
+        this.maxHealth = health;
         this.health = health;
+
     }
 
     public float AddHealth(float health)
     {
         this.health += health;
+
+        if (healthGauge != null)
+        {
+            float calc = this.health / maxHealth;
+            Debug.Log("ca " + calc);
+            healthGauge.size = new Vector2((0.32f) * calc,0.08f);
+        }
+
         return this.health;
     }
+
+
+
 
     public bool IsDie()
     {

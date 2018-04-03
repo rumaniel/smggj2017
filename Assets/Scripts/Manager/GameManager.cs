be6629db4488f412c5f63935ae075e7024d6b274
Ruman <rumaniel@hotmail.com>
2018-04-04 00:35:11 +0900

    refactoring gamemanager

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -8,13 +8,8 @@ public class GameManager : MonoSingleton<GameManager>
 {
 	public bool isPause = false;
 	public List<StageInfo> stageList;
-	public GameObject playButton;
 	public GameObject playerShip;
-	public GameObject hud;
-	public GameObject timerText;
-	//public GameObject enemySpawner;
-	//public GameObject hazardSpawner;
-	//public GameObject powerUpSpawner;
+
     public GameObject SettingDlg;
     public GameObject SettingBtn;
 	public GridManager grid;
@@ -38,57 +33,17 @@ public class GameManager : MonoSingleton<GameManager>
         DOTween.Init(false, true, LogBehaviour.ErrorsOnly);
         // Set the default state to 'Opening'.
         GMState = GameManagerState.Opening;
-		UpdateGameManagerState ();
 		
 		// Init UI
 		OnSettingClose(true);
 		grid.cutscene.CloseCutScene();
 	}
 
-	// Function to switch the current game state.
-	void UpdateGameManagerState()
-	{
-		switch (GMState) 
-		{
-		case GameManagerState.Opening:
-			// Show the cursor.
-			//Cursor.visible = true;
-			// Hide the game over text.
-			// Hide the main HUD text.
-			//hud.SetActive(false);
-			// Display the game title.
-			// GameTitle.SetActive(true);
-			// Show the 'Play' button.
-			playButton.SetActive(true);
-				break;
-		case GameManagerState.InGame:
-			// Hide the cursor.
-			//Cursor.visible = false;
-			//playButton.SetActive(false);
-			//playerShip.GetComponent<PlayerControl>().Init();
-			//StageManager.Instance.StartStage(stageList[0]);
 
-				break;
-		case GameManagerState.GameOver:
-			// Stop the timer.
-			//timerText.GetComponent<TimeCounter>().StopTimeCounter();
-			// Stop the enemy spawner.
-			//enemySpawner.GetComponent<EnemySpawner>().UnscheduleEnemySpawner();
-			// Stop the hazard spawner.
-			//hazardSpawner.GetComponent<HazardSpawner>().UnscheduleHazardSpawner();
-			// Stop the power up spawner.
-			//powerUpSpawner.GetComponent<PowerUpSpawner>().UnschedulePowerUpSpawner();
-			// Display the game over text.
-			// Change the game state to 'Opening' after 5 seconds.
-			Invoke("RestartInGame", 5f);
-			break;
-		}
-	}
 	// This function sets the game state and updates it.
 	public void SetGameManagerState(GameManagerState state)
 	{
 		GMState = state;
-		UpdateGameManagerState ();
 	}
 	// Call this function when the "Play" button is pressed.
 	public void StartInGame(int i)
@@ -98,15 +53,12 @@ public class GameManager : MonoSingleton<GameManager>
             obj.SetActive(false);
         }
 
-
-        playButton.SetActive(false);
         playerShip.GetComponent<PlayerControl>().Init();
 
         StageManager.Instance.StartStage(stageList[i]);
 
 
         GMState = GameManagerState.InGame;
-		UpdateGameManagerState ();
 	}
 	// This will reload the main scene.
 	public void RestartInGame()

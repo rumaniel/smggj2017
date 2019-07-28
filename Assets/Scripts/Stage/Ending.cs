11e602a7b8f7da8d18d4708c603038e5ff232267
Rumaniel <rumaniel@hotmail.com>
2019-07-28 16:53:51 +0900

    Fix warning error

--- Assets/Scripts/Stage/Ending.cs
+++ Assets/Scripts/Stage/Ending.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine;
 using DG.Tweening;
 
 public class Ending : MonoBehaviour {
